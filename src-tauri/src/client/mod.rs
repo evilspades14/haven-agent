ad919c50f00d9606d6343ac68930a77c8d23f814
Jackson <jackson@homegolem.net>
2026-05-07 21:24:37 -0400

    Initial

---
+++ src-tauri/src/client/mod.rs
@@ -0,0 +1,27 @@
+mod model;
+
+use reqwest::Client;
+
+pub struct WallHavenAPIClient {
+    client: Client,
+    base_url: String,
+    api_key: Option<String>
+}
+
+impl WallHavenAPIClient {
+    pub fn build() -> Result<Self, Box<dyn Error>> {
+        let base_url = String::from("https://wallhaven.cc/api");
+        let client = Client::builder().build()?;
+        Ok(Self {
+            client,
+            base_url,
+            api_key: None
+        })
+    }
+
+    pub fn set_api_key(&mut self, api_key: Option<String>) {
+        self.api_key = api_key;
+    }
+
+    
+}
